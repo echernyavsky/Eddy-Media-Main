f81be0c09cebdffa5d49519fe0fae11c59202417
Edward Chernyavsky <gromilich@gmail.com>
2015-05-24 16:46:17 +0300

    Initial commit

---
+++ EddyPromo/js/helpers.js
@@ -0,0 +1,35 @@
+﻿function Helpers() {
+    
+};
+
+Helpers.prototype.showLoader = function() {
+    alert('showing');
+    // TODO
+};
+
+Helpers.prototype.hideLoader = function() {
+    alert('hiding');
+
+    // TODO
+};
+
+Helpers.prototype.submitCallRequest = function (data) {
+    this.showLoader();
+
+    $.ajax({
+        url: '/handleRequestPhoneCall.php',
+        type: 'POST',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        contentType: "application/json;charset=utf-8",
+        success: function() {
+            // TODO: show success toastr message.
+        },
+        error: function() {
+            // TODO: show error message
+        },
+        complete: this.hideLoader,
+    });
+};
+
+var helpers = new Helpers();
---
+++ EddyPromo/js/main.js
@@ -0,0 +1,14 @@
+﻿function submitCallRequest() {
+    // TODO: validate data
+
+    var data = {
+        phone: $('#phone').val(),
+        name: $('#name').val(),
+    };
+
+    helpers.submitCallRequest(data);
+};
+
+$(document).ready(function () {
+    $('#confirm-call-request').on('click', submitCallRequest);
+});
